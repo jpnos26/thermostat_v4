88e23d9199afa3a8927642ba22044ca4434cd9de
Athos <jpnos@gmx.com>
2017-12-12 09:26:38 +0100

    Add upnp t

--- thermostat.py
+++ thermostat.py
@@ -32,19 +32,23 @@
 #                                                                            #
 ##############################################################################
 import threading
-#import math
 import os, os.path, sys
 import time
 import datetime
 import urllib2
 import json
-# import random
 import socket
 import re
 import locale
 locale.setlocale(locale.LC_ALL, '')
 from requests import get
 
+###Upnp Import
+from urlparse import urlparse
+from xml.dom.minidom import parseString
+from xml.dom.minidom import Document
+import httplib
+import argparse
 
 ##############################################################################
 #                                                                            #
@@ -205,7 +209,7 @@ MSG_SUBTYPE_DHT = "DhtWifi"
 #                                                                            #
 ##############################################################################
 
-THERMOSTAT_VERSION = "4.1.2"
+THERMOSTAT_VERSION = "4.1.3"
 
 # Debug settings
 
@@ -928,15 +932,24 @@ def telegramCommand(command,chat_id):
         if command == "/ip":
             f = get("https://api.ipify.org").text
             look_ip = str(f)
+            if settings.get("upnp")["enabled"] == 1:
+                try: 
+                    test_upnp = open_upnp()
+                except: 
+                    test_upnp = 100
             if cherrypy.server.socket_port == 443:
-                bot.sendMessage(chat_id, "Da Internet Thermostat su: https://" + look_ip + "/")
+                    if test_upnp == 200:
+                        bot.sendMessage(chat_id, "Da Internet Thermostat su: https://" + look_ip + ":"+str(settings.get("upnp")["ext_port"]))
+                    else :
+                        bot.sendMessage(chat_id, "Da Internet Thermostat su: https://" + look_ip )
+                        bot.sendMessage(chat_id, "add :port open in you router no upnp ")
             else:
                 bot.sendMessage(chat_id, "Da Internet Thermostat su: http://" + look_ip + "/")
             ip_int = str(get_ip_address())
             if cherrypy.server.socket_port == 443:
-                bot.sendMessage(chat_id, "Da Lan Thermostat su: https://" + ip_int + "/")
+                bot.sendMessage(chat_id, "Da Lan Thermostat su: https://" + ip_int )
             else:
-                bot.sendMessage(chat_id, "Da Lan Thermostat su: http://" + ip_int + "/")
+                bot.sendMessage(chat_id, "Da Lan Thermostat su: http://" + ip_int )
         elif command == "/stato":
             bot.sendMessage(chat_id, test_ip)
         elif command == "/time":
@@ -1333,7 +1346,195 @@ def setLogLevel(msg):
         logLevel = LOG_LEVELS.get(msg.payload, logLevel)
     else:
         log(LOG_LEVEL_ERROR, CHILD_DEVICE_NODE, MSG_SUBTYPE_CUSTOM + "/loglevel", "Invalid LogLevel: " + msg.payload)
+#####################################################################################
+##  UPNP routines
+#####################################################################################
+def open_upnp():
+    res = discover()
+    for path in res:
+        discparsed = urlparse(path)
+        service_path = get_wanip_path(path)
+        service_url = "%s://%s%s"%(discparsed.scheme,discparsed.netloc,service_path)
+        routerip = discparsed.netloc.split(':')[0]
 
+        localip = get_my_ip(routerip)
+
+        dis='Upnp :  '
+
+        status,message = open_port(service_url,settings.get("upnp")["ext_port"],internal_client=localip,internal_port=443,protocol="TCP",duration=settings.get("upnp")["time"],description="thermo_web",enabled = 1)
+        if status==200:
+                print "%sport forward on %s successful, %s->%s:%s"%(dis,routerip,settings.get("upnp")["ext_port"],localip,"443")
+        else:
+            sys.stderr.write("%sport forward on %s failed, status=%s message=%s\n"%(dis,routerip,status,message))
+            allok = False
+        return status
+    
+def discover():
+    """Discover UPNP capable routers in the local network
+    Returns a lit of urls with service descriptions
+    """
+    SSDP_ADDR = "239.255.255.250"
+    SSDP_PORT = 1900
+    SSDP_MX = 2
+    SSDP_ST = "urn:schemas-upnp-org:device:InternetGatewayDevice:1"
+
+    WAIT = 1
+
+    ssdpRequest = "M-SEARCH * HTTP/1.1\r\n" + \
+                    "HOST: %s:%d\r\n" % (SSDP_ADDR, SSDP_PORT) + \
+                    "MAN: \"ssdp:discover\"\r\n" + \
+                    "MX: %d\r\n" % (SSDP_MX, ) + \
+                    "ST: %s\r\n" % (SSDP_ST, ) + "\r\n"
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setblocking(0)
+    sock.sendto(ssdpRequest, (SSDP_ADDR, SSDP_PORT))
+    time.sleep(WAIT)
+    paths = []
+    for _ in range(10):
+        try:
+            data, fromaddr = sock.recvfrom(1024)
+            ip = fromaddr[0]
+            #print "from ip: %s"%ip
+            parsed = re.findall(r'(?P<name>.*?): (?P<value>.*?)\r\n', data)
+
+            # get the location header
+            location = filter(lambda x: x[0].lower() == "location", parsed)
+
+            # use the urlparse function to create an easy to use object to hold a URL
+            router_path = location[0][1]
+            paths.append(router_path)
+
+        except socket.error:
+            '''no data yet'''
+            break
+    return paths
+
+
+def get_wanip_path(upnp_url):
+    # get the profile xml file and read it into a variable
+    directory = urllib2.urlopen(upnp_url).read()
+
+    # create a DOM object that represents the `directory` document
+    dom = parseString(directory)
+
+    # find all 'serviceType' elements
+    service_types = dom.getElementsByTagName('serviceType')
+
+    # iterate over service_types until we get either WANIPConnection
+    # (this should also check for WANPPPConnection, which, if I remember correctly
+    # exposed a similar SOAP interface on ADSL routers.
+    for service in service_types:
+        # I'm using the fact that a 'serviceType' element contains a single text node, who's data can
+        # be accessed by the 'data' attribute.
+        # When I find the right element, I take a step up into its parent and search for 'controlURL'
+        if service.childNodes[0].data.find('WANIPConnection') > 0:
+            path = service.parentNode.getElementsByTagName('controlURL')[0].childNodes[0].data
+            return path
+
+def open_port(service_url,external_port,internal_client,internal_port=None,protocol='TCP',duration=0,description=None,enabled=1):
+    parsedurl = urlparse(service_url)
+
+    if internal_port==None:
+        internal_port = external_port
+
+    if description == None:
+        description = 'generated by port-forward.py'
+
+    if not enabled:
+        duration=1
+
+    doc = Document()
+
+    # create the envelope element and set its attributes
+    envelope = doc.createElementNS('', 's:Envelope')
+    envelope.setAttribute('xmlns:s', 'http://schemas.xmlsoap.org/soap/envelope/')
+    envelope.setAttribute('s:encodingStyle', 'http://schemas.xmlsoap.org/soap/encoding/')
+
+    # create the body element
+    body = doc.createElementNS('', 's:Body')
+
+    # create the function element and set its attribute
+    fn = doc.createElementNS('', 'u:AddPortMapping')
+    fn.setAttribute('xmlns:u', 'urn:schemas-upnp-org:service:WANIPConnection:1')
+
+    # setup the argument element names and values
+    # using a list of tuples to preserve order
+    arguments = [
+        ('NewRemoteHost', ""), # unused - but required
+        ('NewExternalPort', external_port),           # specify port on router
+        ('NewProtocol', protocol),                 # specify protocol
+        ('NewInternalPort', internal_port),           # specify port on internal host
+        ('NewInternalClient', internal_client), # specify IP of internal host
+        ('NewEnabled', enabled),                    # turn mapping ON
+        ('NewPortMappingDescription', description), # add a description
+        ('NewLeaseDuration', duration)]              # how long should it be opened?
+
+    # NewEnabled should be 1 by default, but better supply it.
+    # NewPortMappingDescription Can be anything you want, even an empty string.
+    # NewLeaseDuration can be any integer BUT some UPnP devices don't support it,
+    # so set it to 0 for better compatibility.
+
+    # container for created nodes
+    argument_list = []
+
+    # iterate over arguments, create nodes, create text nodes,
+    # append text nodes to nodes, and finally add the ready product
+    # to argument_list
+    for k, v in arguments:
+        v = str(v)
+        tmp_node = doc.createElement(k)
+        tmp_text_node = doc.createTextNode(v)
+        tmp_node.appendChild(tmp_text_node)
+        argument_list.append(tmp_node)
+
+    # append the prepared argument nodes to the function element
+    for arg in argument_list:
+        fn.appendChild(arg)
+
+    # append function element to the body element
+    body.appendChild(fn)
+
+    # append body element to envelope element
+    envelope.appendChild(body)
+
+    # append envelope element to document, making it the root element
+    doc.appendChild(envelope)
+
+    # our tree is ready, conver it to a string
+    pure_xml = doc.toxml()
+
+    # use the object returned by urlparse.urlparse to get the hostname and port
+    conn = httplib.HTTPConnection(parsedurl.hostname, parsedurl.port)
+
+    # use the path of WANIPConnection (or WANPPPConnection) to target that service,
+    # insert the xml payload,
+    # add two headers to make tell the server what we're sending exactly.
+    conn.request('POST',
+        parsedurl.path,
+        pure_xml,
+        {'SOAPAction': '"urn:schemas-upnp-org:service:WANIPConnection:1#AddPortMapping"',
+         'Content-Type': 'text/xml'}
+    )
+
+    # wait for a response
+    resp = conn.getresponse()
+
+    return resp.status,resp.read()
+
+
+def get_my_ip(routerip=None):
+    if routerip==None:
+        routerip="8.8.8.8" #default route
+    ret = None
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect((routerip,80))
+        ret = s.getsockname()[0]
+        s.close()
+    except:
+        pass
+    return ret
 
    
 
@@ -2369,7 +2570,7 @@ class WebInterface(object):
         file.close()
 
         with thermostatLock:
-            print "set Temperature" ,str(setTemp)
+
             html = html.replace("@@version@@", str(THERMOSTAT_VERSION))
             html = html.replace("@@temp@@", str(setTemp))
             html = html.replace("@@current@@", str(currentTemp))
